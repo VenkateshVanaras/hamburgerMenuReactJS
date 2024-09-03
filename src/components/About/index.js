15813f55f3ff5baf4b47af4f1f082e831c8e80e2
Venkatesh Vanaras <venkatesvanaras7@gmail.com>
2024-09-03 10:35:53 +0000

    hamburger menu application

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import Header from '../Header'
+
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <Header />
+    <div className="about-image-container">
+      <img
+        className="mobile-about-image"
+        alt="about"
+        src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      />
+
+      <img
+        className="desktop-about-image"
+        alt="about"
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      />
+    </div>
+  </div>
+)
+
+export default About
